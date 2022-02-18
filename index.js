db7c3238831d2c3840b6f96085e0549c6de112a9
Arai231456 <96648087+Arai231456@users.noreply.github.com>
2022-02-18 20:36:04 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").innerHTML = "Arailym CS-2119";
+function display()
+{
+  document.getElementById('date').innerHTML = Date();
+}
+document.querySelector("h3").innerHTML = "876 days left until the freedom!"
+function multiplyBy() {
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() {
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
